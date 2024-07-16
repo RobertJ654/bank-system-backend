8d6ccf2740c2bbf80cef5ec354e3ccfc747891f5
Nick <robertjosue654@gmail.com>
2024-07-16 03:14:15 -0400

    home and components

---
+++ config/global.php
@@ -0,0 +1,17 @@
+<?php
+define("CONTROLADOR_DEFECTO", "Usuarios");
+define("ACCION_DEFECTO", "index");
+
+define("RUTA_BASE",$_SERVER['DOCUMENT_ROOT']."/");
+define("HTTP_BASE","http://127.0.0.1/bank");
+define('ROOT_DIR',RUTA_BASE.'bank');
+define('ROOT_CORE',RUTA_BASE.'bank/core');
+define('ROOT_UPLOAD',RUTA_BASE.'bank/uploads');
+define('ROOT_VIEW',RUTA_BASE.'bank/view');
+define('ROOT_REPORT',RUTA_BASE.'bank/reports');
+define('ROOT_REPORT_DOWN',RUTA_BASE.'bank/reports_download');
+define("URL_RESOURCES", HTTP_BASE."/public/");
+//JWT TOKEN
+define('SECRET_KEY','MIEMPRESA.MBmxKMdsfsdffghY7d55sghvTlB1kytftrstews232u6575tdyhrdjyAjB3uNasdasd0g6ZDdOXpz21');  // secret key can be a random string and keep in secret from anyone
+define('ALGORITHM','HS256');   // Algorithm used to sign the token
+?>
